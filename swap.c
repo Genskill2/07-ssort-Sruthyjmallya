9db0d8188b15ed3d97232f070ba5c0a30950b31f
Sruthy J Mallya <53861843+Sruthyjmallya@users.noreply.github.com>
2021-07-01 11:42:18 +0530

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,18 @@
+void swap_max(int arr[],int l,int n){
+  int max = 0,pos;
+  for(int i=n;i<l;i++){
+    if(arr[i]>max){
+      max = arr[i];
+      pos = i;
+    }
+  }
+  temp = arr[n];
+  arr[n] = arr[pos];
+  arr[pos] = temp;
+}
+
+void ssort(int arr[],int l){
+  for(int i=0;i<l-1;i++){
+    swap_max(arr,l,i);
+  }
+}
